7f410c5717de14e659c942aaa1eff0789adb4e87
Andri Sutanto <andrisutantochen@gmail.com>
2022-06-26 09:36:15 +0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,43 +1,14 @@
-.App {
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: #ffefc0;
+// Kalian bisa menambahkan CSS di src/components/Header.css
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Call a Friend</h1>
+            <p>Your friendly contact app</p>
+            <hr/>
+        </header>
+    )
 }
 
-.card {
-  background-color:#ffcc9d;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  width: 20%;
-  margin-bottom: 10px;
-  border-radius: 15px;
-  padding: 20px;
-}
-
-.card>img{
-  border-radius: 50%;
-  max-width: 120px;
-  max-height: 120px;
-}
-
-.description {
-  display: flex;
-  flex-direction: column;
-  width: 40%;
-  margin-left: 20px;
-}
-
-.description p,h4{
-  text-align: left;
-  margin: 5px;
-}
-
-hr {
-  border-top: 5px dotted #ff8080;
-  width: 600px;
-  margin-bottom: 20px;
-}
+export default Header;
